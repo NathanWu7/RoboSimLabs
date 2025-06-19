fff7bf2df51729cd1480e43789bf06f1d45f5e72
wqw <nathan.wuqw@gmail.com>
2025-06-19 14:28:10 +0800

    update isaac

---
+++ Unisaac/source/unisaac/unisaac/tasks/manipulation/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Manipulation environments for fixed-arm robots."""
+
+from .Robot_arm import *  # noqa
+from .Dextrous_hand import *  # noqa
